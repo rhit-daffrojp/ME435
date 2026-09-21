117fae878eae24f8bb30f42d87a68ea7b31004d7
John Daffron <daffrojp@rose-hulman.edu>
2026-09-21 14:34:19 -0400

    serial from pi400 updates for messages

--- Python/Hello Worlds/Lab 3/Initial_pyserial_learning.py
+++ Python/Hello Worlds/Lab 3/Initial_pyserial_learning.py
@@ -1,14 +1,26 @@
 import serial
+import time
 
-print("Learning Pyserial")
+print("Learning Pyserial") # with pi400
 
 ser = serial.Serial(port='/dev/ttyACM0', baudrate = 19200, timeout=10)  # open serial port
-#ser.open()
-while not ser.is_open:
-    print("Opening...")
+time.sleep(2.0) # Necessary sometimes. :)
 
-# TODO: Use the serial object
 
+#ser.open()     # Not necessary
+
+ser.reset_input_buffer()  # flush input buffer, discarding all its contents
+message = "RESET"
+print(message)
+message_bytes = (message + '\n').encode()  # convert to bytes
+print(message_bytes)
+
+ser.write(message_bytes)  # write a string
+
+response_bytes = ser.readline()
+print(response_bytes)
+response = response_bytes.decode().strip()
+print(response)
 
 
 ser.close()  # close serial port
